da0269dc04d49ffd416583874631cd31ce3d7041
GanXiKang <gan0612xk@hotmail.com>
2024-03-09 19:55:53 +0800

    feat ： new script

---
+++ Assets/Script/House/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("CameraMovement")]
+    public Transform target;
+    public float _smoothTime = 0.3f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
