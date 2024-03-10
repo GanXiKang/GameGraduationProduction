3c16c1b847f2253c5f42f42ff18452fc98f19b27
GanXiKang <gan0612xk@hotmail.com>
2024-03-10 20:14:26 +0800

    feat : 編寫測試

--- Assets/Script/House/UIController.cs
+++ Assets/Script/House/UIController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("InteractionButton")]
+    public GameObject interactionButton;
+    public static Text interactionButton_text;
+    public static bool isActive;
+
     void Start()
     {
-        
+        isActive = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isActive)
+        {
+            interactionButton.SetActive(true);
+        }
     }
 }
