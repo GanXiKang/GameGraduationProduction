efa56658c6c10ae243acce041ae27d55c6423664
GanXiKang <gan0612xk@hotmail.com>
2024-03-10 20:46:55 +0800

    feat ： 調整

--- Assets/Script/House/UIController.cs
+++ Assets/Script/House/UIController.cs
@@ -7,7 +7,7 @@ public class UIController : MonoBehaviour
 {
     [Header("InteractionButton")]
     public GameObject interactionButton;
-    public static Text interactionButton_text;
+    public Text interactionButton_text;
     public static bool isActive;
 
     void Start()
