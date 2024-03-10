c212f943070c9fedf53ac65d16c5a9a442b4e20f
GanXiKang <gan0612xk@hotmail.com>
2024-03-10 20:41:31 +0800

    feat : 編寫内容

--- Assets/Script/House/PlayerColliderControl.cs
+++ Assets/Script/House/PlayerColliderControl.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerColliderControl : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UIController.isActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UIController.isActive = false;
+        }
     }
 }
