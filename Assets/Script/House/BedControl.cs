25fe579b6f4f51dd0b5fcaae87b60bd2ee18e68b
GanXiKang <gan0612xk@hotmail.com>
2024-03-12 00:34:54 +0800

    feat ： 編寫code

--- Assets/Script/House/BedControl.cs
+++ Assets/Script/House/BedControl.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BedControl : MonoBehaviour
-{ 
+{
+    public Transform sleepPoint;
+    public GameObject player;
+    public static bool isSleep;
+
     void Start()
     {
-        
+        isSleep = false;
     }
 
     void Update()
     {
-        
+        if (isSleep)
+        {
+            player.transform.position = sleepPoint.position;
+            player.transform.rotation = Quaternion.Euler(-90f, 90f, -270f);
+        }
     }
 }
