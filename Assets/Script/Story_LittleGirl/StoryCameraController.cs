0f122b055d1008fd78097fc379ef0bbe5aadb4df
GanXiKang <gan0612xk@hotmail.com>
2024-03-13 01:49:52 +0800

    feat ： 編寫code

--- Assets/Script/Story_LittleGirl/StoryCameraController.cs
+++ Assets/Script/Story_LittleGirl/StoryCameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class StoryCameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("CameraMovement")]
+    public Transform target;
+    public float _smoothTime = 0.5f;
+    public static bool isFollow;
+    private Vector3 velocity = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        isFollow = true;
+    }
+    
+    void LateUpdate()
+    {
+        if (isFollow)
+        {
+            Vector3 _targetPosition = target.position + new Vector3(0f, 6f, -10f);
+
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref velocity, _smoothTime);
+            transform.rotation = Quaternion.Euler(30f, 0f, 0f);
+        }
     }
 }
