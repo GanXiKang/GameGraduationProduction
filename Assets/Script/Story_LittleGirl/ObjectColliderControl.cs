a3064ce6dfda04ca13c25070681aac4b47e97bea
GanXiKang <gan0612xk@hotmail.com>
2024-03-17 01:40:28 +0800

    feat ： 編寫code

--- Assets/Script/Story_LittleGirl/ObjectColliderControl.cs
+++ Assets/Script/Story_LittleGirl/ObjectColliderControl.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class ObjectColliderControl : MonoBehaviour
 {
+    public int _serialNumber;
+    public static int _nowNumber;
+
+    void Update()
+    {
+        if (StoryLittleGirlUIControl.isInteractionButtonActive)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                StoryLittleGirlUIControl.isInteractionButtonActive = false;
+                switch (_nowNumber)
+                {
+                    case 1:                                                     //clothing
+                        
+                        break;
+
+                    
+                }
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
