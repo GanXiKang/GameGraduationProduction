583f8a80bb51518d228de93ccd7243647e095317
GanXiKang <gan0612xk@hotmail.com>
2024-03-16 18:02:59 +0800

    feat : new script

---
+++ Assets/Script/Story_LittleGirl/StoryBagControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryBagControl : MonoBehaviour
+{
+    bool isBagActive;
+
+    void Start()
+    {
+        isBagActive = false;
+    }
+
+    void Update()
+    {
+        
+    }
+}
