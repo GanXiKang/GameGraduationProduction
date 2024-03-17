32e85b9f9228ba9da49ec1e8105c37154cf557b4
GanXiKang <gan0612xk@hotmail.com>
2024-03-18 01:33:45 +0800

    feat : new script

---
+++ Assets/Item/ItemController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemController : ScriptableObject
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
