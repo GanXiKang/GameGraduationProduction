f73148630f7543dbd78c9118213390e9b346ab4e
GanXiKang <gan0612xk@hotmail.com>
2024-03-18 00:25:41 +0800

    feat : 編寫code

--- Assets/Script/Menu/MenuGameControl.cs
+++ Assets/Script/Menu/MenuGameControl.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuGameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Start_Button()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
