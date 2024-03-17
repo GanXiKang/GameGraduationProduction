c11bce4aa12175f4afe9330e4f94ad3dad530517
GanXiKang <gan0612xk@hotmail.com>
2024-03-18 01:39:22 +0800

    feat ： save script

--- Assets/Item/ItemController.cs
+++ Assets/Item/ItemController.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ItemSystem/Item")]
 public class ItemController : ScriptableObject
 {
-    
+    public int ItemID;
+    public string ItemName;
+    public Sprite ItemSprite;
+
+    public void Use()
+    {
+        //Method to use item
+    }
 }
