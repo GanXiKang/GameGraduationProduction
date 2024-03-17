7dd4a7d4bb2d82d99a4bef8cd5284a20bafcf2d3
GanXiKang <gan0612xk@hotmail.com>
2024-03-18 02:22:20 +0800

    feat : 編寫code

--- Assets/Script/Story_LittleGirl/StoryBagControl.cs
+++ Assets/Script/Story_LittleGirl/StoryBagControl.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryBagControl : MonoBehaviour
 {
+    GameObject[] itemSlot;
     public GameObject bagUI;
 
     bool isBagActive;
@@ -11,6 +13,7 @@ public class StoryBagControl : MonoBehaviour
     void Start()
     {
         isBagActive = false;
+        itemSlot = GameObject.FindGameObjectsWithTag("Item");
     }
 
     void Update()
