48f49b9585759203cdde584fcf3bd4c593b757b3
GanXiKang <gan0612xk@hotmail.com>
2024-03-19 22:32:22 +0800

    feat ： 添加print

--- Assets/Script/Public/BagController.cs
+++ Assets/Script/Public/BagController.cs
@@ -19,6 +19,7 @@ public class BagController : MonoBehaviour
 
         if (MenuGameControl.isNewGameModel)
         {
+            print("1");
             MenuGameControl.isNewGameModel = false;
             for (int i = 0; i < itemSlot.Length; i++)
             {
@@ -40,6 +41,7 @@ public class BagController : MonoBehaviour
 
     void ItemSlotActive()
     {
+        print("2");
         for (int k = 1; k < itemSlot.Length; k++)
         {
             itemSlot[k].SetActive(isItemSlotAcite[k]);
