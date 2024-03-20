3255403230428123ad356493f8313f814e74c075
GanXiKang <gan0612xk@hotmail.com>
2024-03-21 02:01:50 +0800

    feat ： 添加判斷

--- Assets/Script/House/All/PlayerController.cs
+++ Assets/Script/House/All/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue value)
     {
-        if (!CameraController.isLookWorkbench && !UIController.isContentActive && !BedControl.isSleep)
+        if (!CameraController.isLookWorkbench && !UIController.isContentActive && !BedControl.isSleep && !BagController.isBagActive)
         {
             Vector2 input = value.Get<Vector2>();
             _moveInput = new Vector3(input.x, 0f, input.y);
