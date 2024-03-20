d126f6c3afe16e695f50ea990e72ed30052ade77
GanXiKang <gan0612xk@hotmail.com>
2024-03-21 01:47:01 +0800

    feat : 編寫code

---
+++ Assets/Script/House/Outdoor/FlowerControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerControl : MonoBehaviour
+{
+    public static bool isDestory;
+
+    void Start()
+    {
+        isDestory = false;
+    }
+
+    void Update()
+    {
+        if (isDestory)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
