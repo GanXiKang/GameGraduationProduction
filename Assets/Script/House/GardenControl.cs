f4b9fb1561b61fb15c582f322f403f273bea4ce9
GanXiKang <gan0612xk@hotmail.com>
2024-03-21 01:21:38 +0800

    feat : new script

---
+++ Assets/Script/House/GardenControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenControl : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
