d9687e498118a87b50e6265ac22ad49a2e975f8e
GanXiKang <gan0612xk@hotmail.com>
2024-03-21 01:40:48 +0800

    feat ： 優化code

--- Assets/Script/House/GameControl.cs
+++ Assets/Script/House/GameControl.cs
@@ -7,6 +7,40 @@ public class GameControl : MonoBehaviour
 {
     public static int _day = 2;
 
+    void Update()
+    {
+        if (UIController.isInteractionButtonActive)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                UIController.isInteractionButtonActive = false;
+                switch (PlayerColliderControl._nowNumber)
+                {
+                    case 1:                                                     //bed
+                        CameraController.isFollow = false;
+                        CameraController.isLookBed = true;
+                        UIController.isChooseStoryActive = true;
+                        BedControl.isSleep = true;
+                        break;
+
+                    case 2:                                                     //workbench
+                        CameraController.isFollow = false;
+                        CameraController.isLookWorkbench = true;
+                        break;
+
+                    case 3:                                                     //cabinet
+                        UIController.isContentActive = true;
+                        UIController._conveyContentTextNumber = 1;
+                        break;
+
+                    case 4:                                                     //flower
+                        print("flower");
+                        break;
+                }
+            }
+        }
+    }
+
     public void Story_LittleGirl()
     {
         SceneManager.LoadScene(2);
--- Assets/Script/House/PlayerColliderControl.cs
+++ Assets/Script/House/PlayerColliderControl.cs
@@ -7,40 +7,6 @@ public class PlayerColliderControl : MonoBehaviour
     public int _serialNumber;
     public static int _nowNumber;
 
-    void Update()
-    {
-        if (UIController.isInteractionButtonActive)
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                UIController.isInteractionButtonActive = false;
-                switch (_nowNumber)
-                {
-                    case 1:                                                     //bed
-                        CameraController.isFollow = false;
-                        CameraController.isLookBed = true;
-                        UIController.isChooseStoryActive = true;
-                        BedControl.isSleep = true;
-                        break;
-
-                    case 2:                                                     //workbench
-                        CameraController.isFollow = false;
-                        CameraController.isLookWorkbench = true;
-                        break;
-
-                    case 3:                                                     //cabinet
-                        UIController.isContentActive = true;
-                        UIController._conveyContentTextNumber = 1;
-                        break;
-
-                    case 4:                                                     //flower
-                        print("flower");
-                        break;
-                }
-            }
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
