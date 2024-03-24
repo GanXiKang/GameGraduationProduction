474549531b608ae747259b93a8e7b73f0062be56
GanXiKang <gan0612xk@hotmail.com>
2024-03-25 01:25:33 +0800

    feat ： 編寫code

--- Assets/Script/House/LivingRoom/WorkbenchControl.cs
+++ Assets/Script/House/LivingRoom/WorkbenchControl.cs
@@ -4,6 +4,84 @@ using UnityEngine;
 
 public class WorkbenchControl : MonoBehaviour
 {
+    [Header("Drawing")]
     public static bool isDrawing;
- 
+    public GameObject pencil;
+    public BoxCollider boxC;
+    public bool isDrawingComplete;
+    public float _completionThreshold;
+    public float _completionPercentage;
+    float _drawnArea;
+    bool isPress;
+    List<GameObject> pencils = new List<GameObject>(0);
+
+    void Start()
+    {
+        isDrawing = false;
+        isDrawingComplete = false;
+    }
+
+    void Update()
+    {
+        if (isDrawing)
+        {
+            Drawing();
+        }
+    }
+
+    void Drawing()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isPress = true;
+            pencils.Add(Instantiate(pencil, Camera.main.ScreenToWorldPoint(mousePos), Quaternion.Euler(Vector3.zero), this.transform));
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isPress = false;
+        }
+
+        if (!isDrawingComplete && isPress)
+        {
+            if (Drawing_isWithInBoundary(Camera.main.ScreenToWorldPoint(mousePos)))
+            {
+                pencils[pencils.Count - 1].transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+                _drawnArea += _completionPercentage * Time.deltaTime;
+                print(_drawnArea);
+            }
+        }
+
+        Drawing_CheckDrawingCompletion();
+    }
+
+    bool Drawing_isWithInBoundary(Vector2 pos)
+    {
+        Bounds _bounds = boxC.bounds;
+
+        return _bounds.Contains(pos);
+    }
+
+    void Drawing_CheckDrawingCompletion()
+    {
+        if (!isDrawingComplete)
+        {
+            if (_drawnArea >= _completionThreshold)
+            {
+                print("ÀL®‹Íê³É£¡");
+                isDrawingComplete = true;
+                //Drawing_ClearAllPencils();
+            }
+        }
+    }
+
+    void Drawing_ClearAllPencils()
+    {
+        for (int i = 0; i < pencils.Count; i++)
+        {
+            Destroy(pencils[i]);
+        }
+        pencils.Clear();
+    }
 }
