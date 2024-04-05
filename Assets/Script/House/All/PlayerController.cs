d5dc5c8e95284c991e4261ed35742276f6fe2e44
GanXiKang <gan0612xk@hotmail.com>
2024-04-06 01:48:25 +0800

    feat ： 加判斷

--- Assets/Script/House/All/PlayerController.cs
+++ Assets/Script/House/All/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue value)
     {
-        if (!CameraController.isLookWorkbench && !UIController.isContentActive && !BedControl.isSleep && !BagController.isBagActive)
+        if (!CameraController.isLookWorkbench && !UIController.isContentActive && !BedControl.isSleep && !BagController.isBagActive && !GameControl.isOpeningStopMove)
         {
             Vector2 input = value.Get<Vector2>();
             _moveInput = new Vector3(input.x, 0f, input.y);
