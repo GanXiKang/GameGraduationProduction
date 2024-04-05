7d17fd2985c06de1750d208da402d723a3484285
GanXiKang <gan0612xk@hotmail.com>
2024-04-06 01:02:02 +0800

    feat : 編寫code

--- Assets/Script/House/All/DoorControl.cs
+++ Assets/Script/House/All/DoorControl.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class DoorControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform playerPoint;
+    public Transform NextPlacePoint;
+    public int _doorNumber;
+    public static int _whoDoor;
+    public static bool isNextPlace = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isNextPlace)
+        {
+            playerPoint.position = NextPlacePoint.position;
+            isNextPlace = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameControl._day != 1)
+        {
+            if (other.tag == "Player")
+            {
+                _whoDoor = _doorNumber;
+            }
+        }
     }
 }
