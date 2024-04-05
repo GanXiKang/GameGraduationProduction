3aa404d3d8738bc585ebac680539789289a567b6
GanXiKang <gan0612xk@hotmail.com>
2024-04-06 01:54:15 +0800

    feat ： 編寫code

--- Assets/Script/House/Bedroom/DoorControl.cs
+++ Assets/Script/House/Bedroom/DoorControl.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DoorControl : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (GameControl._day == 1)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
