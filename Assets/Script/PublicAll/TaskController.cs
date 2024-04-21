7d3e8a38195e4b0d013b9345a5f0c85d217210e4
GanXiKang <gan0612xk@hotmail.com>
2024-04-21 20:50:08 +0800

    feat ： 編寫code

--- Assets/Script/PublicAll/TaskController.cs
+++ Assets/Script/PublicAll/TaskController.cs
@@ -7,13 +7,17 @@ public class TaskController : MonoBehaviour
 {
     public GameObject taskUI;
     public Text taskText;
+    public TextAsset taskFile;
 
     public static bool isTaskActive;
     public static int _taskNumber = 1;
 
+    List<string> taskList = new List<string>();
+
     void Start()
     {
         isTaskActive = false;
+        GetTextFormFile(taskFile);
     }
 
     void Update()
@@ -30,21 +34,31 @@ public class TaskController : MonoBehaviour
         }
     }
 
+    void GetTextFormFile(TextAsset file)
+    {
+        var lineDate = file.text.Split("\n");
+
+        foreach (var line in lineDate)
+        {
+            taskList.Add(line);
+        }
+    }
     void TaskTextControl()
     {
-        switch (_taskNumber)
-        {
-            case 1:
-                taskText.text = "檢查背包";
-                break;
+        taskText.text = taskList[_taskNumber];
+        //switch (_taskNumber)
+        //{
+        //    case 1:
+        //        taskText.text = "檢查背包";
+        //        break;
 
-            case 2:
-                taskText.text = "查看工作臺";
-                break;
+        //    case 2:
+        //        taskText.text = "查看工作臺";
+        //        break;
 
-            case 3:
-                taskText.text = "到花園采取顔料";
-                break;
-        }
+        //    case 3:
+        //        taskText.text = "到花園采取顔料";
+        //        break;
+        //}
     }
 }
