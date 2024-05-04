714d6f265b58414f28187b15796349b6f34c886b
GanXiKang <gan0612xk@hotmail.com>
2024-05-04 21:36:15 +0800

    feat : 編寫code

---
+++ Assets/Script/PublicAll/DontDestoryControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestoryControl : MonoBehaviour
+{
+    static DontDestroyOnLoadControl instance;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (this != instance)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (StoryGameControl_LittleGirl.isDestoryGameControl)
+        {
+            Destroy(gameObject);
+            StoryGameControl_LittleGirl.isDestoryGameControl = false;
+        }
+    }
+}
