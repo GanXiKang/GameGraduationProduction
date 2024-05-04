3f8a9aff6c1fdecdc4e3514ae42f62a7353b3031
GanXiKang <gan0612xk@hotmail.com>
2024-05-04 21:47:16 +0800

    feat : 更換

--- Assets/Script/PublicAll/DontDestoryControl.cs
+++ Assets/Script/PublicAll/DontDestoryControl.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class DontDestoryControl : MonoBehaviour
 {
-    static DontDestoryControl instance;
-
-    void Start()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else if (this != instance)
-        {
-            Destroy(gameObject);
-        }
-    }
-    void FixedUpdate()
-    {
-        if (StoryGameControl_LittleGirl.isDestoryGameControl)
-        {
-            Destroy(gameObject);
-            StoryGameControl_LittleGirl.isDestoryGameControl = false;
-        }
-    }
+    
 }
--- Assets/Script/Story_LittleGirl/DontDestoryGameControl_LittleGirl.cs
+++ Assets/Script/Story_LittleGirl/DontDestoryGameControl_LittleGirl.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DontDestoryGameControl_LittleGirl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static DontDestoryControl instance;
+
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (this != instance)
+        {
+            Destroy(gameObject);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (StoryGameControl_LittleGirl.isDestoryGameControl)
+        {
+            Destroy(gameObject);
+            StoryGameControl_LittleGirl.isDestoryGameControl = false;
+        }
     }
 }
