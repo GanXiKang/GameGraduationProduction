499beb9d291c71696c5c3c72a278911b6cccec18
GanXiKang <gan0612xk@hotmail.com>
2024-05-08 00:53:02 +0800

    feat ： 添加bool

--- Assets/Script/House/Bedroom/CaseControl.cs
+++ Assets/Script/House/Bedroom/CaseControl.cs
@@ -6,26 +6,35 @@ public class CaseControl : MonoBehaviour
 {
     public GameObject caseCollider;
 
-    [Header("StoryBook")]
+    [Header("StoryBookLitteGirl")]
     public GameObject storyBookLitteGirl;
     public GameObject storyLittleGirl_Button;
-    public static bool isStoryBookLittleGirl;
+    public static bool isStoryBookLittleGirl = true;
+    public static bool isStoryBookLittleGirlFinish = false;
 
+    [Header("StoryBookCrystal")]
     public GameObject storyBookCrystal;
     public GameObject storyCrystal_Button;
-    public static bool isStoryBookCrystal;
+    public static bool isStoryBookCrystal = false;
+    public static bool isStoryBookCrystalFinish = false;
 
+    [Header("StoryBookMomotaro")]
     public GameObject storyBookMomotaro;
     public GameObject storyMomotaro_Button;
-    public static bool isStoryBookMomotaro;
+    public static bool isStoryBookMomotaro = false;
+    public static bool isStoryBookMomotaroFinish = false;
 
+    [Header("StoryBookBeanstalk")]
     public GameObject storyBookBeanstalk;
     public GameObject storyBeanstalk_Button;
-    public static bool isStoryBookBeanstalk;
+    public static bool isStoryBookBeanstalk = false;
+    public static bool isStoryBookBeanstalkFinish = false;
 
+    [Header("StoryBookCinderella")]
     public GameObject storyBookCinderella;
     public GameObject storyCinderella_Button;
-    public static bool isStoryBookCinderella;
+    public static bool isStoryBookCinderella = false;
+    public static bool isStoryBookCinderellaFinish = false;
 
     void Start()
     {
