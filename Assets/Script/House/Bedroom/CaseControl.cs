df3218aa876e0d3173b8c019185ac104401ecef6
GanXiKang <gan0612xk@hotmail.com>
2024-05-08 00:43:43 +0800

    feat : 編寫變數

--- Assets/Script/House/Bedroom/CaseControl.cs
+++ Assets/Script/House/Bedroom/CaseControl.cs
@@ -6,6 +6,27 @@ public class CaseControl : MonoBehaviour
 {
     public GameObject caseCollider;
 
+    [Header("StoryBook")]
+    public GameObject storyBookLitteGirl;
+    public GameObject storyLittleGirl_Button;
+    public static bool isStoryBookLittleGirl;
+
+    public GameObject storyBookCrystal;
+    public GameObject storyCrystal_Button;
+    public static bool isStoryBookCrystal;
+
+    public GameObject storyBookMomotaro;
+    public GameObject storyMomotaro_Button;
+    public static bool isStoryBookMomotaro;
+
+    public GameObject storyBookBeanstalk;
+    public GameObject storyBeanstalk_Button;
+    public static bool isStoryBookBeanstalk;
+
+    public GameObject storyBookCinderella;
+    public GameObject storyCinderella_Button;
+    public static bool isStoryBookCinderella;
+
     void Start()
     {
         switch (GameControl._day)
