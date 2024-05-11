0c80764cd125e9bfe23f1dec75b3a287eec20d99
GanXiKang <gan0612xk@hotmail.com>
2024-05-12 04:46:40 +0800

    feat ： 編寫code

--- Assets/Script/PublicAll/SettingControl.cs
+++ Assets/Script/PublicAll/SettingControl.cs
@@ -5,17 +5,27 @@ using UnityEngine.UI;
 
 public class SettingControl : MonoBehaviour
 {
+    AudioSource BGM;
+
     public Image bookUI;
+    public Slider sliderBGM;
+    public Toggle fullScreen;
     public GameObject[] settingUI;
     public Button[] labelColor;
     public Sprite[] turnPage;
 
+    public static float volumeBGM = 0.7f;
+    public static bool isFullS;
     public static int _page = 1;
 
     bool isUIInteractable;
 
     void Start()
     {
+        BGM = GetComponent<AudioSource>();
+
+        sliderBGM.value = volumeBGM;
+        fullScreen.isOn = Screen.fullScreen;
         isUIInteractable = true;
     }
 
@@ -66,6 +76,16 @@ public class SettingControl : MonoBehaviour
     public void LabelRed_Button()               //êPé]
     {
         settingUI[0].SetActive(false);
+    }
+    public void Volume_BGM()
+    {
+        volumeBGM = sliderBGM.value;
+        BGM.volume = volumeBGM;
+    }
+    public void FullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+        isFullS = isFullScreen;
     }
 
     //·­•ø„Ó®‹
