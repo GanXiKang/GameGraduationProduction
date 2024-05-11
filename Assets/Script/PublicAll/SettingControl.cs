264aa5e82c03b49bee46566878c97ccf88313ecf
GanXiKang <gan0612xk@hotmail.com>
2024-05-12 02:56:24 +0800

    feat ： 編寫code

--- Assets/Script/PublicAll/SettingControl.cs
+++ Assets/Script/PublicAll/SettingControl.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class SettingControl : MonoBehaviour
 {
-    public static Sprite[] turnPage;
+    public GameObject settingUI;
+    public Image bookUI;
+    public Sprite[] turnPage;
 
     void Start()
     {
@@ -20,16 +22,18 @@ public class SettingControl : MonoBehaviour
 
     IEnumerator TurnPageLeft()
     {
-
+        bookUI.sprite = turnPage[1];
         yield return new WaitForSeconds(0.5f);
-
+        bookUI.sprite = turnPage[2];
         yield return new WaitForSeconds(0.5f);
+        bookUI.sprite = turnPage[0];
     }
     IEnumerator TurnPageRight()
     {
-
+        bookUI.sprite = turnPage[3];
         yield return new WaitForSeconds(0.5f);
-
+        bookUI.sprite = turnPage[4];
         yield return new WaitForSeconds(0.5f);
+        bookUI.sprite = turnPage[0];
     }
 }
