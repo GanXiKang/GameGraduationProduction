1d13262a78e9d8e02264f6cbe80d4ceb056551ef
GanXiKang <gan0612xk@hotmail.com>
2024-05-12 17:56:21 +0800

    feat ： 編寫code

--- Assets/Script/Menu/MenuGameControl.cs
+++ Assets/Script/Menu/MenuGameControl.cs
@@ -5,11 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuGameControl : MonoBehaviour
 {
+    public GameObject settingUI;
+    public GameObject interactiveUI;
+
     public static bool isNewGameModel = true; //•º•rµÄ
+    public static bool isSettingUI = false;
+
+    void Update()
+    {
+        settingUI.SetActive(isSettingUI);
+        interactiveUI.SetActive(!isSettingUI);
+    }
 
     public void Start_Button()
     {
         isNewGameModel = true;
         SceneManager.LoadScene(1);
+    }
+    public void Setting_Button()
+    {
+        isSettingUI = true;
     }
 }
