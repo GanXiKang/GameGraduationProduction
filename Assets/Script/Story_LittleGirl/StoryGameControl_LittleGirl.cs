36e7c36f8be8a2b0c6ac81efa42b95cc8926ce12
GanXiKang <gan0612xk@hotmail.com>
2024-05-13 02:45:34 +0800

    feat ： 編寫code

--- Assets/Script/Story_LittleGirl/StoryGameControl_LittleGirl.cs
+++ Assets/Script/Story_LittleGirl/StoryGameControl_LittleGirl.cs
@@ -12,6 +12,7 @@ public class StoryGameControl_LittleGirl : MonoBehaviour
     public static int _task = 0;
     public static int _chapter = 0;
     public static bool isStartStoryContent = false;
+    public static bool isChangeScene;
     bool once;
 
     //Chapter1
@@ -93,6 +94,7 @@ public class StoryGameControl_LittleGirl : MonoBehaviour
                         break;
 
                     case 4:                                                               //GoAlley
+                        isChangeScene = true;
                         loadingUI.SetActive(true);
                         Invoke("CloseLoadingUI", 4f);
                         break;
@@ -205,6 +207,7 @@ public class StoryGameControl_LittleGirl : MonoBehaviour
     }
     void CloseLoadingUI()
     {
+        isChangeScene = false;
         loadingUI.SetActive(false);
         chapter[1].SetActive(false);
         chapter[2].SetActive(true);  
