922c3bdc9a6437092486071715d979d20516948f
GanXiKang <gan0612xk@hotmail.com>
2024-05-14 02:29:09 +0800

    feat : 編寫code

--- Assets/Script/Story_LittleGirl/StoryArrowTipControl_LittleGirl.cs
+++ Assets/Script/Story_LittleGirl/StoryArrowTipControl_LittleGirl.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class StoryArrowTipControl_LittleGirl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 0.5f;
+    Vector3 startPos;
+    Vector3 endPos;
+    bool isMoveUp;
+
+    public static bool isDestory = false;
+
     void Start()
     {
-        
+        startPos = transform.position;
+        endPos = startPos + new Vector3(0, 0.8f, 0);
+        isMoveUp = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isDestory)
+        {
+            if (isMoveUp)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPos, speed * Time.deltaTime);
+                if (transform.position == endPos)
+                {
+                    isMoveUp = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
+                if (transform.position == startPos)
+                {
+                    isMoveUp = true;
+                }
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
