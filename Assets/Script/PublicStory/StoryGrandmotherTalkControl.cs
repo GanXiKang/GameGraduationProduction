fe0e969e059d6483e14b6ab41c0a4beedeea8b3e
GanXiKang <gan0612xk@hotmail.com>
2024-05-15 00:46:41 +0800

    feat : 製作動畫

--- Assets/Script/PublicStory/StoryGrandmotherTalkControl.cs
+++ Assets/Script/PublicStory/StoryGrandmotherTalkControl.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryGrandmotherTalkControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image grandmother;
+    public Sprite[] image;
+
+    bool isNewStartAnimator;
+
     void Start()
     {
-        
+        isNewStartAnimator = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isNewStartAnimator)
+        {
+            StartCoroutine(GrandmotherTalk());
+        }
+    }
+
+    IEnumerator GrandmotherTalk()
+    {
+        isNewStartAnimator = false;
+        yield return new WaitForSeconds(1f);
+        grandmother.sprite = image[1];
+        yield return new WaitForSeconds(1f);
+        grandmother.sprite = image[0];
+        yield return new WaitForSeconds(1f);
+        grandmother.sprite = image[2];
+        yield return new WaitForSeconds(1f);
+        grandmother.sprite = image[0];
+        isNewStartAnimator = true;
     }
 }
