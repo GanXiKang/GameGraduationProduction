86575fae1bc17aa0deeaafb66ca90326014f6a19
GanXiKang <gan0612xk@hotmail.com>
2024-05-15 23:16:06 +0800

    feat ： 編寫code

--- Assets/Script/House/All/LightControl.cs
+++ Assets/Script/House/All/LightControl.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class LightControl : MonoBehaviour
 {
-    public float rotationSpeed = 5f; 
+    public GameObject night;
+    public GameObject daytime;
+    public GameObject roof;
+
+    public MeshRenderer catLamp;
+    public Material catLamp_Light;
+    public Material catLamp_noLight;
 
     void Update()
     {
-        //transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        night.SetActive(BedControl.isNight);
+        daytime.SetActive(BedControl.isNight);
+        roof.SetActive(BedControl.isNight);
+
+        if (BedControl.isNight)
+        {
+            catLamp.material = catLamp_Light;
+        }
+        else
+        {
+            catLamp.material = catLamp_noLight;
+        }
     }
 }
