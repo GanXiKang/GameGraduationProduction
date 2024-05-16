6b46737ec3ebdf8abfa94569fe50e56d5cd60cc3
GanXiKang <gan0612xk@hotmail.com>
2024-05-17 00:34:27 +0800

    feat ： 編寫code

--- Assets/Script/Menu/MenuGameControl.cs
+++ Assets/Script/Menu/MenuGameControl.cs
@@ -5,22 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MenuGameControl : MonoBehaviour
 {
+    [Header("Musia")]
+    public AudioClip onClick;
+    AudioSource BGM;
+
+    [Header("UI")]
     public GameObject interactiveUI;
 
     public static bool isNewGameModel = true; //•º•rµÄ
 
     void Update()
     {
+        BGM = GetComponent<AudioSource>();
+
         interactiveUI.SetActive(!SettingControl.isSettingActive);
     }
 
     public void Start_Button()
     {
+        BGM.PlayOneShot(onClick);
         isNewGameModel = true;
         SceneManager.LoadScene(1);
     }
     public void Setting_Button()
     {
+        BGM.PlayOneShot(onClick);
         SettingControl.isSettingActive = true;
     }
 }
