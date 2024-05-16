d667ba1afa9a83c35906312a436ea58b2025cf23
GanXiKang <gan0612xk@hotmail.com>
2024-05-17 03:02:52 +0800

    feat : 編寫code

--- Assets/Script/PublicAll/EffectsSoundControl.cs
+++ Assets/Script/PublicAll/EffectsSoundControl.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class EffectsSoundControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    AudioSource BGM;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        BGM.volume = SettingControl.volumeBGM;
     }
 }
