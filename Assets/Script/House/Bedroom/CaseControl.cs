d1e2ce2b72bdf512d5f316288f842d1a087f4835
GanXiKang <gan0612xk@hotmail.com>
2024-05-18 02:27:58 +0800

    feat ： 測試結束 delete//code

--- Assets/Script/House/Bedroom/CaseControl.cs
+++ Assets/Script/House/Bedroom/CaseControl.cs
@@ -61,7 +61,7 @@ public class CaseControl : MonoBehaviour
 
     void Update()
     {
-        //StoryBookFinish();
+        StoryBookFinish();
         ChooseStoryBookButtonisHighlighter();
     }
 
