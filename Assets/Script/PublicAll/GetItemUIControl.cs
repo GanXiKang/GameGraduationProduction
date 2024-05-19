1cf89eb80a9614368b268c73f28461ff1638cec3
GanXiKang <gan0612xk@hotmail.com>
2024-05-20 01:00:20 +0800

    feat : 編寫code

---
+++ Assets/Script/PublicAll/GetItemUIControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetItemUIControl : MonoBehaviour
+{
+    public GameObject getItemUI;
+    public GameObject panel;
+    public GameObject theme;
+    public Image item;
+    public static bool isGetItemActice;
+
+    void Start()
+    {
+        isGetItemActice = false;
+    }
+
+    void Update()
+    {
+        
+    }
+}
