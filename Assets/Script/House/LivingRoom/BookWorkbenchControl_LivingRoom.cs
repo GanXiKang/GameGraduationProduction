b495defdbbada159ec5db8cd78c8009e29a463e4
GanXiKang <gan0612xk@hotmail.com>
2024-06-14 01:04:52 +0800

    feat  : copy code

--- Assets/Script/House/LivingRoom/BookWorkbenchControl_LivingRoom.cs
+++ Assets/Script/House/LivingRoom/BookWorkbenchControl_LivingRoom.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class BookWorkbenchControl_LivingRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator anim;
+
+    public GameObject[] quizImage;
+    public static bool isOpenBook;
+    public static bool isCloseBook;
+    public static bool isFinishQuiz = false;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        anim.SetInteger("isChapter", StoryGameControl_LittleGirl._chapter);
+        anim.SetBool("isOpenBook", isOpenBook);
+        anim.SetBool("isCloseBook", isCloseBook);
+
+        if (isOpenBook)
+        {
+            Invoke("FalseOfOpenBook", 0.2f);
+        }
+        if (isCloseBook)
+        {
+            Invoke("FalseOfCloseBook", 0.2f);
+        }
+
+        quizImage[0].SetActive(!isFinishQuiz);
+        quizImage[1].SetActive(isFinishQuiz);
+        quizImage[2].SetActive(isFinishQuiz);
+    }
+
+    void FalseOfOpenBook()
+    {
+        isOpenBook = false;
+    }
+    void FalseOfCloseBook()
+    {
+        isCloseBook = false;
     }
 }
