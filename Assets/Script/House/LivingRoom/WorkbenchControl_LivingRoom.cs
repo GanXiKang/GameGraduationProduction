80f5757b667c253c8815b313f6b66275de508387
GanXiKang <gan0612xk@hotmail.com>
2024-06-14 00:59:27 +0800

    feat ： rename

--- Assets/Script/House/LivingRoom/WorkbenchControl_LivingRoom.cs
+++ Assets/Script/House/LivingRoom/WorkbenchControl_LivingRoom.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Workbench_LivingRoom : MonoBehaviour
+public class WorkbenchControl_LivingRoom : MonoBehaviour
 {
     [Header("Musia")]
     public AudioClip complete;
