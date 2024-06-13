747ba3ff3a08b644db6abe3f24243731a59af81f
GanXiKang <gan0612xk@hotmail.com>
2024-06-14 01:38:19 +0800

    feat : copy

--- Assets/Script/House/Bedroom/BookCaseControl_Bedroom.cs
+++ Assets/Script/House/Bedroom/BookCaseControl_Bedroom.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class BookCaseControl_Bedroom : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator anim;
+
+    public int _storyNumber;
+    public static int _chooseWhichStoryWorld;
+    public static bool isChooseWhichStoryBook;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isChooseWhichStoryBook)
+        {
+            switch (_storyNumber)
+            {
+                case 1:
+                    if (_storyNumber == _chooseWhichStoryWorld)
+                    {
+                        anim.SetBool("isChooseStoryBook", true);
+                        CaseControl_Bedroom.isStoryBookLittleGirl = false;
+                    }
+                    break;
+
+                case 2:
+                    if (_storyNumber == _chooseWhichStoryWorld)
+                    {
+                        anim.SetBool("isChooseStoryBook", true);
+                        CaseControl_Bedroom.isStoryBookCrystal = false;
+                    }
+                    break;
+            }
+            Invoke("AutoFalseOfisChooseWhichStoryBook", 0.5f);
+        }
+    }
+
+    void AutoFalseOfisChooseWhichStoryBook()
+    {
+        isChooseWhichStoryBook = false;
     }
 }
