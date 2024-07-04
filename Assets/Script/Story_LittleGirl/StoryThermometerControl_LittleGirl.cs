c4186151672a023d61e5537556a5c0c79c1d3113
GanXiKang <gan0612xk@hotmail.com>
2024-07-04 22:29:21 +0800

    feat : 編寫code

--- Assets/Script/Story_LittleGirl/StoryThermometerControl_LittleGirl.cs
+++ Assets/Script/Story_LittleGirl/StoryThermometerControl_LittleGirl.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryThermometerControl_LittleGirl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static bool isThermometer = false;
+    public static bool isDead;
+    float _temperature;
+    float _decline;
+    float _rise;
+
+    [Header("UI")]
+    public GameObject thermometerUI;
+    public Image energyBar;
+
     void Start()
     {
-        
+        _temperature = 36.5f;
+        _decline = 0.1f;
+        _rise = 0.15f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        thermometerUI.SetActive(isThermometer);
+        energyBar.fillAmount = (_temperature - 35f) / 2;
+
+        if (isThermometer)
+        {
+            if (!StoryGameControl_LittleGirl.isUseMatches)
+            {
+                _temperature -= _decline * Time.deltaTime;
+            }
+            else 
+            {
+                _temperature += _rise * Time.deltaTime;
+            }
+        }
+
+        Limit();
+    }
+
+    void Limit()
+    {
+        if (_temperature >= 37.0f)
+        {
+            _temperature = 37.0f;
+        }
+
+        if (_temperature <= 35.0f)
+        {
+            _temperature = 35.0f;
+            isDead = true;
+        }
     }
 }
