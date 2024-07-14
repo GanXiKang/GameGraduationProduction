49da5376e50113a8713ddb854455585a1dd5bc61
GanXiKang <gan0612xk@hotmail.com>
2024-07-14 20:59:13 +0800

    feat : 優化code

--- Assets/Script/House/All/PlayerControl_House.cs
+++ Assets/Script/House/All/PlayerControl_House.cs
@@ -59,7 +59,7 @@ public class PlayerControl_House : MonoBehaviour
         {
             Vector2 input = value.Get<Vector2>();
             _moveInput = new Vector3(input.x, 0f, input.y);
-            _lookDirection = new Vector3(_moveInput.x, 0f, _moveInput.z).normalized;
+            _lookDirection = new Vector3(input.x, 0f, input.y).normalized;
         }
         else
         {
